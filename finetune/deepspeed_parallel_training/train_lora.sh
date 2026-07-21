b03316a323c4c9d7784ffb093e0f5c911307166c
hjy <j.han@student.tue.nl>
2026-07-21 11:02:47 +0800

    (q)lora training attempts

---
+++ finetune/deepspeed_parallel_training/train_lora.sh
@@ -0,0 +1,38 @@
+accelerate launch --config_file "/root/autodl-tmp/oki-agent/finetune/deepspeed_parallel_training/deepspeed_config.yaml"  /root/autodl-tmp/oki-agent/finetune/deepspeed_parallel_training/train.py \
+--seed 100 \
+--model_name_or_path "/root/autodl-tmp/models/qwen3.6-35b-a3b" \
+--dataset_name "smangrul/ultrachat-10k-chatml" \
+--chat_template_format "chatml" \
+--add_special_tokens False \
+--append_concat_token False \
+--splits "train,test" \
+--max_length 400 \
+--num_train_epochs 1 \
+--logging_steps 5 \
+--log_level "info" \
+--logging_strategy "steps" \
+--eval_strategy "epoch" \
+--save_strategy "epoch" \
+--bf16 True \
+--packing True \
+--learning_rate 1e-4 \
+--lr_scheduler_type "cosine" \
+--weight_decay 1e-4 \
+--warmup_steps 0 \
+--max_grad_norm 1.0 \
+--output_dir "/root/autodl-tmp/models/deepspeed_lora" \
+--per_device_train_batch_size 1 \
+--per_device_eval_batch_size 1 \
+--gradient_accumulation_steps 1 \
+--gradient_checkpointing True \
+--use_reentrant False \
+--dataset_text_field "content" \
+--use_flash_attn True \
+--use_peft_lora True \
+--lora_r 8 \
+--lora_alpha 16 \
+--lora_dropout 0.1 \
+--lora_target_modules "out_proj,in_proj_qkv,in_proj_z,in_proj_b,in_proj_a,q_proj,k_proj,v_proj,o_proj" \
+--use_4bit_quantization False \
+--max_steps 20
+
