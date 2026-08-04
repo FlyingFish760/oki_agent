3c3f88c5c68853374672fd44aabc59a466903c4d
hjy <j.han@student.tue.nl>
2026-08-04 16:28:58 +0800

    now start formal lora training

--- finetune/run_train_lora.sh
+++ finetune/run_train_lora.sh
@@ -1,11 +1,10 @@
 export HF_ENDPOINT="https://hf-mirror.com"
 
-accelerate launch --config_file "/root/autodl-tmp/oki-agent/finetune/deepspeed_parallel_training/accelerate_config.yaml" /root/autodl-tmp/oki-agent/finetune/train_lora_test.py \
+accelerate launch --config_file "/root/autodl-tmp/oki-agent/finetune/deepspeed_parallel_training/accelerate_config.yaml" /root/autodl-tmp/oki-agent/finetune/train_lora.py \
     --model_name_or_path /root/autodl-tmp/models/qwen3.6-35b-a3b \
     --custom_train_data_path /root/autodl-tmp/oki-agent/finetune/data/datasets/privacy_security_train.jsonl \
     --custom_eval_data_path /root/autodl-tmp/oki-agent/finetune/data/datasets/privacy_security_valid.jsonl \
     --learning_rate 2.0e-5 \
-    --max_steps 5 \
     --warmup_steps 0.03 \
     --num_train_epochs 2 \
     --packing False \
@@ -15,11 +14,11 @@ accelerate launch --config_file "/root/autodl-tmp/oki-agent/finetune/deepspeed_p
     --eos_token '<|im_end|>' \
     --logging_steps 5 \
     --eval_strategy steps \
-    --eval_steps 0.5 \
+    --eval_steps 0.2 \
     --use_peft \
     --lora_r 16 \
     --lora_alpha 32 \
     --lora_target_modules in_proj_qkv in_proj_z out_proj q_proj k_proj v_proj o_proj \
     --output_dir /root/autodl-tmp/models/trl_deepspeed_lora_adapter \
     --non_thinking_training True \
-    2>&1 | tee /root/autodl-tmp/oki-agent/finetune/run_train_lora_test_08_04.log
+    2>&1 | tee /root/autodl-tmp/oki-agent/finetune/run_train_lora_08_04.log
